f8a6567f632e16f90d130b0f4d53765237c51892
gilanggymnastiar <gilanggymnastiarr@gmail.com>
2023-04-05 21:21:59 +0700

    Template

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Gilang</li>
+ <li>Nama Belakang : Gymnastiar</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Binong Permai</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Tangerang</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>Volly</li>
+ <li>Futsal</li>
+ </ul>
+</section>
